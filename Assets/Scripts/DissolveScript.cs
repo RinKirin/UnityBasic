50b59a180fa606ecb4c33bf0aa4b1c912eeb5a68
RinKirin <rin0kirin@gmail.com>
2020-04-20 21:22:28 +0300

    tutorial #2

---
+++ Assets/Scripts/DissolveScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveScript : MonoBehaviour
+{
+    public float timer = 0.0f;
+    public bool _dissolve = false;
+    public Material mat;
+    public float matFl = 0;
+
+
+    public float dissolveTime = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0.0f;
+        mat = this.gameObject.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            _dissolve = true;
+            timer = 0;
+            mat.SetFloat("_dissolvePower", timer - 1);
+        }
+        
+        if (_dissolve)
+        {
+            if (timer < 2)
+            {
+                timer += Time.deltaTime*(2/dissolveTime);
+                mat.SetFloat("_dissolvePower", timer-1);
+            }
+            else
+            {
+                _dissolve = false;
+               //this.gameObject.active = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private float time = 0.0f;
+    private Material mat;
+
+    public float disTime = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0.0f;
+        mat = this.gameObject.GetComponent<Renderer>().material;
+    }
+
+    private bool _dis = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            time = 0.0f;
+            _dis = true;
+        }
+
+        if (_dis)
+        {
+            if (time < 2)
+            {
+                time += Time.deltaTime * (2 / disTime);
+                mat.SetFloat("_dissolvePower", time-1);
+            }
+            else
+            {
+                time = 0.0f;
+                _dis = false;
+            }
+            
+        }
+    }
+}
